da407b3a8b8f81e6565b4f84baa90bfe0f6f7da1
srinivasanrk1 <srinivasrk.mail@gmail.com>
2022-12-28 23:10:48 +0530

    initial commit

---
+++ samplecpp.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string first_name, last_name;
+    cout<<"sample program";
+    
+    cout<<"Enter first name";
+    cin>>first_name;
+    cout<<"Enter Last name";
+    cin>>last_name;
+    cout<<"Entered name is " << first_name + last_name;
+    return 0;
+}
